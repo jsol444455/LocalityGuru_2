85d8e6e5e9decc3390fb8f08435283f8df8bc6bb
jsol444455 <j.sol.444455@gmail.com>
2026-03-02 17:25:40 -0600

    Update run_graph_seg.py

--- run_graph_seg.py
+++ run_graph_seg.py
@@ -4,15 +4,15 @@ from paver_runtime_integration import PAVERPipeline, GPUArchitecture, Partitioni
 pipeline = PAVERPipeline()
 
 # Load your LocalityGuru output
-pipeline.load_locality_data("/home/m/LocalityGuru_2/img/bicg/_Z12bicg_kernel2PfS_S__2-1_matrix.json")
+pipeline.load_locality_data("/home/m/LocalityGuru_2/img/gemm/_Z11gemm_kernelPfS_S__16-64_matrix.json")
 # OR load matrix directly
 # pipeline.load_locality_matrix(locality_matrix, kernel_name="GEMM")
 
 # Configure GPU
-pipeline.configure_gpu(GPUArchitecture.VOLTA, max_tb_per_sm=8)
+pipeline.configure_gpu(GPUArchitecture.VOLTA, max_tb_per_sm=8, num_sms=8)
 
 # Run partitioning (RB-TS recommended)
-result = pipeline.run_partitioning(PartitioningMethod.KWAY_TS)
+result = pipeline.run_partitioning(PartitioningMethod.TSP_TS)
 
 # Export config for GPGPU-Sim
 pipeline.export_scheduler_config("paver_config.txt")
